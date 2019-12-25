8553a7bd6c64f2313059012068b086b322b7fb7b
eg_b <egbv.py@gmail.com>
2019-12-25 23:15:23 +0300

    brain_games.py was modified: run function was added

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,14 @@
-#!/home/eg/.pyenv/versions/3.8.1/bin
+#!/home/eg/python_project
+
+import sys
+sys.path.append('/home/eg/python_projects/python-project-lvl1/brain_games')
+
+from cli import run
 
 def main():
     print("Welcome to the Brain Games")
+
+run()
 
 if __name__ == '__main__':
     main()
