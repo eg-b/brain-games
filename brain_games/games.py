5ec5d532bf9aced46864c30c44111c84bbb78518
eg_b <egbv.py@gmail.com>
2020-01-03 18:16:39 +0300

    games.py was added (even/odd game logic)

---
+++ brain_games/games.py
@@ -0,0 +1,48 @@
+from brain_games.scripts.brain_games import main
+import random
+import prompt
+import brain_games.cli
+
+
+def generate_question():
+    num = random.randint(1, 100)
+    print('Question: {}'.format(num))
+    return num
+
+
+def ask_question():
+    user_answer = prompt.string('Your answer: ')
+    return user_answer
+
+
+def is_even(num):
+    if num % 2 == 0:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return correct_answer
+
+
+def check_answer(user_answer, correct_answer):
+    if user_answer == correct_answer:
+        return True      
+    else:
+        return False 
+
+
+def game():
+    user_name = brain_games.cli.run()
+    answ_counter = 0
+    for i in range(3):
+        num = generate_question()
+        user_answer = ask_question()
+        correct_answer = is_even(num)
+        check_answer(user_answer, correct_answer)
+        if check_answer(user_answer, correct_answer) is not True:
+            print("'{}' is wrong answer ;(. Correct answer was '{}'. Let's try again, {}!".format(user_answer, correct_answer, user_name))
+            break
+        else: 
+            print('Correct!')
+            answ_counter += 1
+    if answ_counter == 3:
+        print('Congratulations, {}!'.format(user_name))
