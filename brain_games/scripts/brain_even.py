babcc34ec860963317886852a1449ef117e34832
eg_b <egbv.py@gmail.com>
2020-01-03 18:18:08 +0300

    Scripts: brain_even was added

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/home/eg/python_project
+from brain_games.games import game
+
+
+def main():
+    print("Welcome to the Brain Games")
+    print('Answer "yes" if number even otherwise answer "no". \n')
+    game()
+
+if __name__ == '__main__':
+    main()
