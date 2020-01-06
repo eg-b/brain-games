631c6c70a898674740199be6620b1c1f1660e24c
eg_b <egbv.py@gmail.com>
2020-01-06 17:27:14 +0300

    brain_gcd.py script add

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/home/eg/python_project
+from brain_games.games import gcd_game
+from brain_games.game_engine import run_game
+
+
+def main():
+    run_game(gcd_game)
+
+
+if __name__ == '__main__':
+    main()
